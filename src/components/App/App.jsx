cccb219d470e426a28465d5bb859eba6089fba05
BlessingEmejulu <blessingnneamaka57@gmail.com>
2024-09-10 10:16:13 -0700

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../Navbar/Navbar.jsx"
+import Main from "../Main/Main.jsx"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
